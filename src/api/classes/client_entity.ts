e4dea4c719856240bbe608888c1aa1eb8dd07499
DauntlessStudio <caleb@cookingblock.com>
2025-03-12 19:44:52 -0500

    Fixed Issue Where SHORTNAME Keyword Was Processed After NAME Keyword

--- src/api/classes/client_entity.ts
+++ src/api/classes/client_entity.ts
@@ -46,9 +46,9 @@ export class MinecraftClientEntity implements ModuleWriteable {
 
     private static encode(entity: MinecraftClientEntity, options: ProjectOptions): Uint8Array {
         let content = JSON.stringify(entity.entity, null, "\t");
-        content = keywordReplacer(content, options);
         content = content.replace(/SHORTNAME/g, entity.Shortname);
         content = content.replace(/IDENTIFIER/g, entity.Identifier);
+        content = keywordReplacer(content, options);
 
         return new TextEncoder().encode(content);
     }
